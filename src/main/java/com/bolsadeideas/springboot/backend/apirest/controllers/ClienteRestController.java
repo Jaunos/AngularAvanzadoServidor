4a848c4444a92dc0eac5d2b860be5e626c95e71f
A743197 <juan.lopezd.external@atos.net>
2019-03-29 21:27:16 +0100

    first commit

---
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -0,0 +1,29 @@
+package com.bolsadeideas.springboot.backend.apirest.controllers;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
+import com.bolsadeideas.springboot.backend.apirest.models.services.IClienteService;
+
+//Implementa Cors y especifica el dominio. Soporta lista de dominios permitidos(Específico para Angular)
+@CrossOrigin(origins = { "http://localhost:4200" })
+//Anotación para transformar el controller en controller rest
+@RestController
+@RequestMapping("/api")
+public class ClienteRestController {
+
+	@Autowired
+	private IClienteService clienteService;
+
+	@GetMapping("/clientes")
+	public List<Cliente> index() {
+		return clienteService.findAll();
+	}
+
+}
