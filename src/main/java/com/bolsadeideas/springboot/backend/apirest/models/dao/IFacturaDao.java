94a4096e72f1e7697f0cc12814c1d6267abec5fa
Juan J. Lopez <juancorvelle@gmail.com>
2019-04-17 20:31:18 +0200

    Carpeta 17 Video 19

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/dao/IFacturaDao.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/dao/IFacturaDao.java
@@ -1,9 +1,12 @@
 package com.bolsadeideas.springboot.backend.apirest.models.dao;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Factura;
 
+@Transactional
 public interface IFacturaDao extends CrudRepository<Factura, Long>{
 
 }
--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/dao/IProductoDao.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/dao/IProductoDao.java
@@ -9,10 +9,14 @@ import com.bolsadeideas.springboot.backend.apirest.models.entity.Producto;
 
 public interface IProductoDao extends CrudRepository<Producto, Long>{
 
+	// Busca todos los productos que contengan el valor elegido
 	@Query("select p from Producto p where p.nombre like %?1%")
 	public List<Producto> findByNombre(String term);
 	
+	// Busca en cualquier parte de la cadena 
 	public List<Producto> findByNombreContainingIgnoreCase(String term);
 	
+	
+	// Busca el producto que comience con...
 	public List<Producto> findByNombreStartingWithIgnoreCase(String term);
 }
